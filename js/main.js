60cfd7b80dde4dc5dbec82b8729c87aa1e20118e
Szymon Cieśla <cszymon01@gmail.com>
2022-07-20 15:46:33 +0200

    changed nav, styled cards in about us section

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const navBtn = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav__menu')
 const navItems = document.querySelectorAll('.nav__item')
 const navDropdown = document.querySelector('.nav__dropdown')
+const navDropdownSecond = document.querySelector('.nav__dropdown-second')
 const DropdownItems = document.querySelectorAll('.nav__item-dropdown')
 const trigger = document.querySelector('.trigger')
+const triggerSecond = document.querySelector('.trigger-second')
 const overlay = document.querySelector('.nav__overlay')
 
 const handleNav = () => {
@@ -14,6 +16,9 @@ const handleNav = () => {
 const handleDropdown = () => {
 	navDropdown.classList.toggle('nav__dropdown--active')
 }
+const handleDropdownSecond = () => {
+	navDropdownSecond.classList.toggle('nav__dropdown-second--active')
+}
 
 const openBtn = () => {
 	navBtn.classList.toggle('is-active')
@@ -22,3 +27,4 @@ const openBtn = () => {
 navBtn.addEventListener('click', handleNav)
 navBtn.addEventListener('click', openBtn)
 trigger.addEventListener('click', handleDropdown)
+triggerSecond.addEventListener('click', handleDropdownSecond)
